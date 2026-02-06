1a73be365444e0bd1b5e06333938383c841d72a5
osama-elnaggar <osama.tawfik04@eng-st.cu.edu.eg>
2026-02-06 22:54:04 +0200

    ..

--- WhatsDown/src/main/java/group/el/de7k/WhatsDown/Controller/UserController.java
+++ WhatsDown/src/main/java/group/el/de7k/WhatsDown/Controller/UserController.java
@@ -3,14 +3,10 @@ package group.el.de7k.WhatsDown.Controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import group.el.de7k.WhatsDown.dto.UserRegisterRequestDto;
-import group.el.de7k.WhatsDown.dto.UserResponoseDto;
 import group.el.de7k.WhatsDown.models.User;
 import group.el.de7k.WhatsDown.service.UserService;
 
@@ -27,7 +23,12 @@ public class UserController {
 
     @GetMapping("/users")
     public List<User> getAllUsers() {
+
+        userService.getAllUsers().forEach(user -> {
+            System.out.println("User: " + user.getEmail());
+        });
         return userService.getAllUsers();
+
     }
 
     @GetMapping("/users/{id}")
